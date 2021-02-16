f5fb700f6afe1f4722181fd25aa24d4014fded57
root <root@localhost.localdomain>
2021-02-16 01:45:38 +0100

    modification de l'exercice 2

--- exercice3.sh
+++ exercice3.sh
@@ -4,9 +4,9 @@ username=$1
 password=$2
 
 #Je recupere les uid des utilisateur ici 
-for i in $(cat /etc/passwd | cut -d ":" -f 1) ;                                  
+for i in $(cat /etc/passwd | cut -d ":" -f 1);                                
 do  
-    #Je vérifie si l'uid existe
+    #Je vérifie si l'utilisateur existe
     if [ $i = $username ]; then 
        echo "L'utilisateur \"$i\" existe déjà"
        exit 0
@@ -15,7 +15,7 @@ do
         if [ -z $username ] && [ -z $username ]; then
 	  echo "Vous devez passer le login et le mot de passe en paramètre du script"
 	else
-	  sudo adduser -c "$username" -m "$username" -p "$password" 
+	  sudo useradd -c "$username" -m "$username" -p "$password" 
 	fi
        exit 0
     fi  
